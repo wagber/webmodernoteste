4eddfebb9ae40d7e203fa3db779eda3a22e2e39e
Wagber <wagber.dev@gmail.com>
2020-02-20 20:04:28 -0300

    Fila não convencional

---
+++ fila.js
@@ -0,0 +1,18 @@
+const fila = []
+fila.unshift(1)
+fila.unshift(2)
+fila.unshift(3)
+fila.unshift(4)
+
+//imprimindo a fila
+console.log(fila)
+
+//tirando o primeiro da fila com POP
+console.log(fila.pop())
+console.log(fila)
+console.log(fila.pop())
+console.log(fila)
+console.log(fila.pop())
+console.log(fila)
+console.log(fila.pop())
+console.log(fila)
