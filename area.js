ab648dfed629b25ec2939d9e9e25f204b25d2ebe
Wagber <wagber.dev@gmail.com>
2020-02-21 00:00:17 -0300

    Funções de Area

---
+++ area.js
@@ -0,0 +1,29 @@
+function calcularArea(l, a){
+    console.log(l * a)
+}
+
+calcularArea(2, 5)
+
+function calcularArea(l, a){
+    return l * a
+}
+
+console.log(calcularArea(2, 3))
+
+calcularArea = (l, a) => {
+    return l * a
+}
+
+console.log(calcularArea(3,8))
+
+calcularArea = (l, a) => {
+    console.log(l * a)
+}
+
+calcularArea(3,7)
+
+let area = function (l, a){
+    return l * a
+}
+console.log(area(4,10))
+
